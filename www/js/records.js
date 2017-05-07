3c6383903fa63f158c8bc9f08e6f6c7ac43ac245
Hristo Gatsinski <h.gatsinski@magicsolutions.bg>
2017-05-07 09:34:44 +0300

    Add overall statistics

--- www/js/records.js
+++ www/js/records.js
@@ -39,8 +39,7 @@ function Record(date, distanceTraveled, fuelUsed, description) {
     };
     this.addLeadingZeroes = function (number) {
         return ("00" + number).substr(-2,2);
-
-    }
+    };
 }
 
 
@@ -64,39 +63,55 @@ function RecordList(list, selector) {
         if (selector == undefined) {
             selector = this.selector;
         }
+        var record, totalFuel, totalDistance;
+        totalFuel = totalDistance = 0;
+
         var currentDate = new Date();
         var currentMonth = currentDate.getMonth();
         var recordDate;
 
         $(selector).empty();
         for(var i = 0; i < this.list.length; i++) {
-            recordDate = new Date(this.list[i].date);
+            record = this.list[i];
+            recordDate = new Date(record.date);
             if (recordDate.getMonth() === currentMonth) {
-                this.list[i].addToDOM(selector);
+                totalFuel += Number(record.fuelUsed);
+                totalDistance += Number(record.distanceTraveled);
+                record.addToDOM(selector);
             }
         }
-    }
+        this.addOverall(selector, totalDistance, totalFuel);
+    };
     this.showThisYearOnly = function(selector) {
         if (selector == undefined) {
             selector = this.selector;
         }
+        var record, totalFuel, totalDistance;
+        totalFuel = totalDistance = 0;
+
         var currentDate = new Date();
         var currentYear = currentDate.getFullYear();
         var recordDate;
 
         $(selector).empty();
         for(var i = 0; i < this.list.length; i++) {
-            recordDate = new Date(this.list[i].date);
+            record = this.list[i];
+            recordDate = new Date(record.date);
 
             if (recordDate.getFullYear() === currentYear) {
-                this.list[i].addToDOM(selector);
+                totalFuel += Number(record.fuelUsed);
+                totalDistance += Number(record.distanceTraveled);
+                record.addToDOM(selector);
             }
         }
-    }
+        this.addOverall(selector, totalDistance, totalFuel);
+    };
     this.showFromTo = function(from, to, selector) {
         if (selector == undefined) {
             selector = this.selector;
         }
+        var record, totalFuel, totalDistance;
+        totalFuel = totalDistance = 0;
 
         var fromDate = new Date(from);
         var toDate = new Date(to);
@@ -105,11 +120,31 @@ function RecordList(list, selector) {
         $(selector).empty();
 
         for(var i = 0; i < this.list.length; i++) {
-            recordDate = new Date(this.list[i].date)
+            record = this.list[i];
+            recordDate = new Date(record.date)
 
             if (recordDate >= fromDate && recordDate <= toDate) {
-                this.list[i].addToDOM(selector);
+                totalFuel += Number(record.fuelUsed);
+                totalDistance += Number(record.distanceTraveled);
+                record.addToDOM(selector);
             }
         }
-    }
+        this.addOverall(selector, totalDistance, totalFuel);
+    };
+    this.addOverall = function(tbody, totalFuel, totalDistance) {
+        /**
+         * Adds overall into the DOM
+         * @param {string} tbody
+         * @param {number} totalFuel
+         * @param {number} totalDistance
+         */
+
+        $tbody = $(tbody);
+
+        $tr = $('<tr>')
+        $tr.append('<td> Total </td>');
+        $tr.append('<td>' + totalDistance + '</td>');
+        $tr.append('<td>' + totalFuel + '</td>');
+        $tbody.append($tr);
+    };
 }
