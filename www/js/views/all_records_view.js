6dc286d7e83516ae013424e80f1b567a7a2fb20a
Hristo Gatsinski <h.gatsinski@magicsolutions.bg>
2017-06-05 20:33:06 +0300

    Add export option

--- www/js/views/all_records_view.js
+++ www/js/views/all_records_view.js
@@ -35,3 +35,34 @@ $('body').on('input', '#to_date, #from_date', function() {
         recordList.showFromTo(from_date, to_date);
     }
 });
+
+$('body').on('click', '#export_table', function() {
+    exportToExcel();
+});
+
+function exportToExcel() {
+    var htmls = "";
+    var uri = 'data:application/vnd.ms-excel;base64,';
+    var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'; 
+    var base64 = function(s) {
+        return window.btoa(unescape(encodeURIComponent(s)))
+    };
+
+    var format = function(s, c) {
+        return s.replace(/{(\w+)}/g, function(m, p) {
+            return c[p];
+        })
+    };
+
+
+    var ctx = {
+        worksheet : 'Worksheet',
+        table : $('#records_table').html()
+    }
+
+
+    var link = document.createElement("a");
+    link.download = "fuel_records.xls";
+    link.href = uri + base64(format(template, ctx));
+    link.click();
+}
